3645ef47ffc46a0776b186ccfede57d57bb61a9f
khus158 <159310438+khus158@users.noreply.github.com>
2025-04-11 12:24:40 +0530

    Update TBL_AMS_User.sql

--- TBL_AMS_User.sql
+++ TBL_AMS_User.sql
@@ -471,3 +471,52 @@ join
 ) as y
 
 on x.grade = y.grade where x.salary > y.AVG_SALARY;
+
+
+
+
+
+
+
+
+
+-----------------------------------------------------------------------------------------------
+
+create proc TestMultiInput
+@Input varchar(max)
+as
+begin
+end
+
+exec TestMultiInput '111:2222', '123:677', '890:900'
+
+-----sample output
+--AcountID AcountNo
+--111      2222
+--123      677
+--890      900
+
+IF OBJECT_ID('tempdb..#tempInput') IS NOT NULL
+DROP TABLE #tempInput;
+
+create table #tempInput 
+(
+  input varchar(200)
+)
+
+insert into #tempInput values('111:2222'), ('123:677')
+
+declare @temp varchar(100)
+set @temp = (select top 1 input from #tempInput)
+while @temp is not null
+begin
+    print @temp 
+	delete from #tempInput where input = @temp
+    set @temp = (select top 1 input from #tempInput)
+end
+
+DROP TABLE #tempInput;
+
+
+
+  
