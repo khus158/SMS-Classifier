005491fcfb7c7bede0a0518960f8f39ea333d9d5
khus158 <159310438+khus158@users.noreply.github.com>
2025-04-11 12:38:13 +0530

    Update TBL_AMS_User.sql

--- TBL_AMS_User.sql
+++ TBL_AMS_User.sql
@@ -517,6 +517,44 @@ end
 
 DROP TABLE #tempInput;
 
+_--------------+
 
+CREATE PROCEDURE TestMultiInput
+    @Input VARCHAR(MAX)
+AS
+BEGIN
+    DECLARE @Str VARCHAR(100), @AccountID VARCHAR(100), @AccountNo VARCHAR(100)
+
+    -- Table to store result
+    DECLARE @Result TABLE (
+        AccountID VARCHAR(100),
+        AccountNo VARCHAR(100)
+    );
+
+    -- Loop to split input by comma
+    WHILE CHARINDEX(',', @Input) > 0
+    BEGIN
+        SET @Str = LEFT(@Input, CHARINDEX(',', @Input) - 1);
+
+        SET @AccountID = LEFT(@Str, CHARINDEX(':', @Str) - 1);
+        SET @AccountNo = RIGHT(@Str, LEN(@Str) - CHARINDEX(':', @Str));
+
+        INSERT INTO @Result VALUES (@AccountID, @AccountNo);
+
+        SET @Input = STUFF(@Input, 1, CHARINDEX(',', @Input), '');
+    END
+
+    -- Handle last item
+    IF LEN(@Input) > 0 AND CHARINDEX(':', @Input) > 0
+    BEGIN
+        SET @AccountID = LEFT(@Input, CHARINDEX(':', @Input) - 1);
+        SET @AccountNo = RIGHT(@Input, LEN(@Input) - CHARINDEX(':', @Input));
+
+        INSERT INTO @Result VALUES (@AccountID, @AccountNo);
+    END
+
+    -- Final output
+    SELECT * FROM @Result;
+END
 
   
